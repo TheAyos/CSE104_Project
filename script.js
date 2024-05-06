24cfc846866bdc8fb2adc6f4ff5d1d4ae718127d
TheAyos <ayosiiiofficiel@gmail.com>
2024-05-06 23:23:13 +0200

    basic interaction done (cell colouring, deactivation & reactivation of start & end radio inputs when such cells are set & unset)

--- script.js
+++ script.js
@@ -1,91 +1,199 @@
-"use strict;"
-const GRID_SIZE = 42; // maybe use dict if conflict 
+"use strict;";
 
+const MIN_CELL_SIZE = 20; // used to create array representing the cells (chose constant size for performance reasons)
 
-const pixelArray = []
-const canvas = document.getElementById('here');
+let cellSize = 42; // maybe use dict if conflict
+
+//TODO: fix bugs in input state variables when alt-tabbing from the window
+
+// TODO: refactor later into obj
+let isMouseDown = false;
+let isShiftDown = false;
+
+let isStartSet = false;
+let isEndSet = false;
+
+let pixelArray = [];
+// matrix : level 1 is y
+//          level 2 is x
+// at [y][x] contains {type: /"obstacle", "free", "start", "finish"/}
+
+const canvas = document.getElementById("here");
 const ctx = canvas.getContext("2d");
+
+const startRadio = document.querySelector('.control-panel>div>input[name="cell_type"]#start');
+const endRadio = document.querySelector('.control-panel>div>input[name="cell_type"]#end');
+
+const getActiveRadio = () => document.querySelector('.control-panel>div>input[name="cell_type"]:checked');
+const getCellSize = () => document.querySelector('.control-panel>div>input[name="cell_size"]').value;
 init();
 
 function init() {
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+  handleResize();
 
-  ySize = canvas.height / GRID_SIZE
-  xSize = canvas.width / GRID_SIZE
+  initControls();
+  initGrid();
+  initEvents();
+
+  draw();
+}
+
+function initControls() {
+  // reset control panel state
+  isStartSet = false;
+  isEndSet = false;
+  startRadio.disabled = false;
+  endRadio.disabled = false;
+  startRadio.checked = true;
+}
+
+function initGrid() {
+  // (re)construct array
+  pixelArray = [];
+  ySize = canvas.height / MIN_CELL_SIZE;
+  xSize = canvas.width / MIN_CELL_SIZE;
   for (let y = 0; y <= ySize; y++) {
-    pixelArray.push([])
+    pixelArray.push([]);
     for (let x = 0; x <= xSize; x++) {
-      pixelArray[y].push(0)
+      pixelArray[y].push({ type: "free" });
     }
   }
+}
 
-  window.addEventListener('mousemove', handleMouse, false);
+function initEvents() {
+  window.addEventListener("resize", handleResize, false);
 
-  window.addEventListener('resize', handleResize, false);
-  handleResize(canvas, ctx);
+  window.addEventListener("mouseup", handleMouse, false);
+  window.addEventListener("mousedown", handleMouse, false);
+  window.addEventListener("mousemove", handleMouse, false);
+
+  window.addEventListener("keydown", handleKeys, false);
+  window.addEventListener("keyup", handleKeys, false);
 }
 
 function draw() {
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  // clear canvas
+  ctx.fillStyle = "rgba(255,255,255,0.1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let y = 0; y < pixelArray.length; y++) {
     for (let x = 0; x < pixelArray[0].length; x++) {
-      console.log(pixelArray[0][0])
-      drawGridUnit(ctx, x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, pixelArray[y][x])
+      cellSize = getCellSize();
+      drawGridUnit(ctx, x * cellSize, y * cellSize, cellSize, pixelArray[y][x].type);
     }
   }
 
-  console.log(pixelArray)
-
-}
-
-function handleMouse(e) {
-  pixelArray[0][1]=1
-
-  // console.log(e.clientX, e.clientY)
-  // console.log(e.offsetX, e.offsetY)
-  let r = canvas.getBoundingClientRect();
-  let insideCanvasMouseX = e.clientX - r.left;
-  let insideCanvasMouseY = e.clientY - r.top;
-
-  if (insideCanvasMouseX < 0 || insideCanvasMouseX > r.width) {
-    insideCanvasMouseX = -1;
-  }
-  if (insideCanvasMouseY < 0 || insideCanvasMouseY > r.height) {
-    insideCanvasMouseY = -1;
-  }
-  console.log(insideCanvasMouseX, insideCanvasMouseY)
+  requestAnimationFrame(draw);
 }
 
 function drawGridUnit(ctx, x, y, size, state) {
   // separate function should allow for easy rendering customization
-  if (state === 1) {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(x+2, y+2, x + size, y + size);
-  } else {
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = '2';
-    ctx.strokeRect(x+2, y+2, x + size, y + size);
+  ctx.beginPath();
+
+  // draw grid
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = "1";
+  ctx.strokeRect(x, y, x + size, y + size);
+
+  if (state !== "free") {
+    if (state === "obstacle") {
+      ctx.fillStyle = "rgba(0,0,255,0.3)";
+    } else if (state === "start") {
+      ctx.fillStyle = "rgba(0,255,0,0.3)";
+    } else if (state === "end") {
+      ctx.fillStyle = "rgba(255,0,0,0.3)";
+    }
+    ctx.fillRect(x, y, size, size);
+  }
+  ctx.closePath();
+}
+
+function handleMouse(e) {
+  // window-to-canvas coordinate mapping
+  const r = canvas.getBoundingClientRect();
+  let insideCanvasMouseX = e.clientX - r.left;
+  let insideCanvasMouseY = e.clientY - r.top;
+
+  // do nothing if mouse outside of canvas ??? is it the behaviour i want ?
+  if (insideCanvasMouseX < 0 || insideCanvasMouseX > r.width) insideCanvasMouseX = undefined;
+  if (insideCanvasMouseY < 0 || insideCanvasMouseY > r.height) insideCanvasMouseY = undefined;
+
+  // actual handling
+  if (
+    ((e.type === "mousemove" && isMouseDown) || e.type === "mousedown") &&
+    insideCanvasMouseX !== undefined &&
+    insideCanvasMouseY !== undefined
+  ) {
+    // console.log(insideCanvasMouseX, insideCanvasMouseY)
+
+    cellIndexY = Math.floor(insideCanvasMouseY / cellSize);
+    cellIndexX = Math.floor(insideCanvasMouseX / cellSize);
+    // console.log(cellIndexY, cellIndexX)
+
+    const currentPixel = pixelArray[cellIndexY][cellIndexX];
+
+    if (isShiftDown) {
+      console.log("shifting");
+      if (currentPixel.type === "start") {
+        isStartSet = false;
+        startRadio.disabled = false;
+      } else if (currentPixel.type === "end") {
+        isEndSet = false;
+        endRadio.disabled = false;
+      }
+      currentPixel.type = "free";
+    }
+    // do not override start and end
+    else if (currentPixel.type !== "start" && currentPixel.type != "end") {
+      console.log("not shifting");
+      const type = getActiveRadio().value;
+      if (type === "start") {
+        if (!isStartSet) {
+          currentPixel.type = type;
+          isStartSet = true;
+          startRadio.disabled = true;
+          document.querySelectorAll(
+            '.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)'
+          )[0].checked = true; // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
+        }
+      } else if (type === "end") {
+        if (!isEndSet) {
+          currentPixel.type = type;
+          isEndSet = true;
+          endRadio.disabled = true;
+          document.querySelectorAll(
+            '.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)'
+          )[0].checked = true; // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
+        }
+      } else {
+        currentPixel.type = type;
+      }
+    }
+  }
+
+  if (e.type === "mousedown") {
+    isMouseDown = true;
+  } else if (e.type === "mouseup") {
+    isMouseDown = false;
   }
 }
 
-function handleResize(canvas, ctx) {
-  // make grid look nice (no unfinished grid cells)
-  let wantedW = window.innerWidth * 2 / 3;
-  let wantedH = window.innerHeight * 2 / 3;
+function handleKeys(e) {
+  // if (e.key === ) {
 
-  // +3 allows to see full border lines of last row and last col grid cells
-  let fixedW = wantedW - (wantedW % GRID_SIZE) + 3;
-  let fixedH = wantedH - (wantedH % GRID_SIZE) + 3;
-  
-  canvas.width = fixedW;
-  canvas.height = fixedH;
-
-  // canvas.width = 400;
-  // canvas.height = 200;
-  draw(canvas, ctx);
+  // }
+  isShiftDown = e.type === "keydown";
 }
 
-requestAnimationFrame(draw)
+function handleResize() {
+  // make grid look nice (no unfinished grid cells)
+  let wantedW = (window.innerWidth * 2) / 3;
+  let wantedH = (window.innerHeight * 2) / 3;
+
+  // // // // +3 allows to see full border lines of last row and last col grid cells
+  let fixedW = wantedW - (wantedW % cellSize);
+  let fixedH = wantedH - (wantedH % cellSize);
+
+  canvas.width = fixedW;
+  canvas.height = fixedH;
+}
