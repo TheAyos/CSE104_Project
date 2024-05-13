0f26a8bf5123e7185fc9ff7c5aa2dd000fd69fa6
TheAyos <ayosiiiofficiel@gmail.com>
2024-05-13 12:08:30 +0200

    first working version !

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict;";
 
-const MIN_CELL_SIZE = 20; // used to create array representing the cells (chose constant size for performance reasons)
+import { initGrid, drawGridUnit, bfs } from "./helper.js";
 
+const MIN_CELL_SIZE = 20; // used to create array representing the cells (chose constant size for performance reasons)
 let cellSize = 42; // maybe use dict if conflict
 
 //TODO: fix bugs in input state variables when alt-tabbing from the window
@@ -13,6 +14,14 @@ let isShiftDown = false;
 let isStartSet = false;
 let isEndSet = false;
 
+// const state = {
+//   isMouseDown: false,
+//   isShiftDown: false,
+
+//   isStartSet: false,
+//   isEndSet: false,
+// };
+
 let pixelArray = [];
 // matrix : level 1 is y
 //          level 2 is x
@@ -23,177 +32,177 @@ const ctx = canvas.getContext("2d");
 
 const startRadio = document.querySelector('.control-panel>div>input[name="cell_type"]#start');
 const endRadio = document.querySelector('.control-panel>div>input[name="cell_type"]#end');
+const clearBtn = document.getElementById("btn_clear_grid");
 
 const getActiveRadio = () => document.querySelector('.control-panel>div>input[name="cell_type"]:checked');
 const getCellSize = () => document.querySelector('.control-panel>div>input[name="cell_size"]').value;
+
 init();
 
 function init() {
-  handleResize();
+    handleResize();
 
-  initControls();
-  initGrid();
-  initEvents();
+    initControls();
+    pixelArray = initGrid(pixelArray, canvas, MIN_CELL_SIZE);
+    initEvents();
 
-  draw();
+    draw();
 }
 
 function initControls() {
-  // reset control panel state
-  isStartSet = false;
-  isEndSet = false;
-  startRadio.disabled = false;
-  endRadio.disabled = false;
-  startRadio.checked = true;
+    // reset control panel state
+    isStartSet = false;
+    startRadio.disabled = false;
+    startRadio.checked = true;
+
+    endRadio.disabled = false;
+    isEndSet = false;
 }
 
-function initGrid() {
-  // (re)construct array
-  pixelArray = [];
-  ySize = canvas.height / MIN_CELL_SIZE;
-  xSize = canvas.width / MIN_CELL_SIZE;
-  for (let y = 0; y <= ySize; y++) {
-    pixelArray.push([]);
-    for (let x = 0; x <= xSize; x++) {
-      pixelArray[y].push({ type: "free" });
-    }
-  }
+function clearGrid() {
+    initControls();
+    pixelArray = initGrid(pixelArray, canvas, MIN_CELL_SIZE);
 }
 
 function initEvents() {
-  window.addEventListener("resize", handleResize, false);
+    window.addEventListener("resize", handleResize, false);
 
-  window.addEventListener("mouseup", handleMouse, false);
-  window.addEventListener("mousedown", handleMouse, false);
-  window.addEventListener("mousemove", handleMouse, false);
+    window.addEventListener("mouseup", handleMouse, false);
+    window.addEventListener("mousedown", handleMouse, false);
+    window.addEventListener("mousemove", handleMouse, false);
+    // disable right click context menu
+    window.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        return false;
+    });
 
-  window.addEventListener("keydown", handleKeys, false);
-  window.addEventListener("keyup", handleKeys, false);
-}
+    window.addEventListener("keydown", handleKeys, false);
+    window.addEventListener("keyup", handleKeys, false);
 
-function draw() {
-  // clear canvas
-  ctx.fillStyle = "rgba(255,255,255,0.1)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  for (let y = 0; y < pixelArray.length; y++) {
-    for (let x = 0; x < pixelArray[0].length; x++) {
-      cellSize = getCellSize();
-      drawGridUnit(ctx, x * cellSize, y * cellSize, cellSize, pixelArray[y][x].type);
-    }
-  }
-
-  requestAnimationFrame(draw);
-}
-
-function drawGridUnit(ctx, x, y, size, state) {
-  // separate function should allow for easy rendering customization
-  ctx.beginPath();
-
-  // draw grid
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = "1";
-  ctx.strokeRect(x, y, x + size, y + size);
-
-  if (state !== "free") {
-    if (state === "obstacle") {
-      ctx.fillStyle = "rgba(0,0,255,0.3)";
-    } else if (state === "start") {
-      ctx.fillStyle = "rgba(0,255,0,0.3)";
-    } else if (state === "end") {
-      ctx.fillStyle = "rgba(255,0,0,0.3)";
-    }
-    ctx.fillRect(x, y, size, size);
-  }
-  ctx.closePath();
-}
-
-function handleMouse(e) {
-  // window-to-canvas coordinate mapping
-  const r = canvas.getBoundingClientRect();
-  let insideCanvasMouseX = e.clientX - r.left;
-  let insideCanvasMouseY = e.clientY - r.top;
-
-  // do nothing if mouse outside of canvas ??? is it the behaviour i want ?
-  if (insideCanvasMouseX < 0 || insideCanvasMouseX > r.width) insideCanvasMouseX = undefined;
-  if (insideCanvasMouseY < 0 || insideCanvasMouseY > r.height) insideCanvasMouseY = undefined;
-
-  // actual handling
-  if (
-    ((e.type === "mousemove" && isMouseDown) || e.type === "mousedown") &&
-    insideCanvasMouseX !== undefined &&
-    insideCanvasMouseY !== undefined
-  ) {
-    // console.log(insideCanvasMouseX, insideCanvasMouseY)
-
-    cellIndexY = Math.floor(insideCanvasMouseY / cellSize);
-    cellIndexX = Math.floor(insideCanvasMouseX / cellSize);
-    // console.log(cellIndexY, cellIndexX)
-
-    const currentPixel = pixelArray[cellIndexY][cellIndexX];
-
-    if (isShiftDown) {
-      console.log("shifting");
-      if (currentPixel.type === "start") {
-        isStartSet = false;
-        startRadio.disabled = false;
-      } else if (currentPixel.type === "end") {
-        isEndSet = false;
-        endRadio.disabled = false;
-      }
-      currentPixel.type = "free";
-    }
-    // do not override start and end
-    else if (currentPixel.type !== "start" && currentPixel.type != "end") {
-      console.log("not shifting");
-      const type = getActiveRadio().value;
-      if (type === "start") {
-        if (!isStartSet) {
-          currentPixel.type = type;
-          isStartSet = true;
-          startRadio.disabled = true;
-          document.querySelectorAll(
-            '.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)'
-          )[0].checked = true; // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
-        }
-      } else if (type === "end") {
-        if (!isEndSet) {
-          currentPixel.type = type;
-          isEndSet = true;
-          endRadio.disabled = true;
-          document.querySelectorAll(
-            '.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)'
-          )[0].checked = true; // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
-        }
-      } else {
-        currentPixel.type = type;
-      }
-    }
-  }
-
-  if (e.type === "mousedown") {
-    isMouseDown = true;
-  } else if (e.type === "mouseup") {
-    isMouseDown = false;
-  }
-}
-
-function handleKeys(e) {
-  // if (e.key === ) {
-
-  // }
-  isShiftDown = e.type === "keydown";
+    clearBtn.addEventListener("click", clearGrid);
 }
 
 function handleResize() {
-  // make grid look nice (no unfinished grid cells)
-  let wantedW = (window.innerWidth * 2) / 3;
-  let wantedH = (window.innerHeight * 2) / 3;
+    // make grid look nice (no unfinished grid cells)
+    let wantedW = (window.innerWidth * 3) / 4;
+    let wantedH = (window.innerHeight * 3) / 4;
 
-  // // // // +3 allows to see full border lines of last row and last col grid cells
-  let fixedW = wantedW - (wantedW % cellSize);
-  let fixedH = wantedH - (wantedH % cellSize);
+    let fixedW = wantedW - (wantedW % cellSize);
+    let fixedH = wantedH - (wantedH % cellSize);
 
-  canvas.width = fixedW;
-  canvas.height = fixedH;
+    canvas.width = fixedW;
+    canvas.height = fixedH;
+}
+
+function handleKeys(e) {
+    isShiftDown = e.type === "keydown" && e.key === "Shift";
+
+    if (e.type === "keydown" && e.key === "c") clearGrid();
+
+    if (e.type === "keydown" && e.key === " ") {
+        // TODO: OOP Pixels class with methods like getStartCell() ... !!!!
+        let startX = -1;
+        let startY = -1;
+        console.log(pixelArray, startX);
+        for (let y = 0; y < pixelArray.length; y++) {
+            for (let x = 0; x < pixelArray[0].length; x++) {
+                console.log(y, x);
+                if (pixelArray[y][x].type === "start") {
+                    startX = x;
+                    startY = y;
+                    break;
+                }
+            }
+            if (startX !== -1) break;
+        }
+
+        console.log("fini !, j'ai:", startX, startY);
+
+        bfs(pixelArray, startX, startY);
+    }
+}
+
+function draw() {
+    // clear canvas
+    ctx.fillStyle = "rgba(255,255,255,0.1)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let y = 0; y < pixelArray.length; y++) {
+        for (let x = 0; x < pixelArray[0].length; x++) {
+            cellSize = getCellSize();
+            drawGridUnit(ctx, x * cellSize, y * cellSize, cellSize, pixelArray[y][x].type);
+        }
+    }
+
+    requestAnimationFrame(draw);
+}
+
+function getPixelFromWindowCoords(x, y) {
+    // window-to-canvas coordinate mapping
+    const r = canvas.getBoundingClientRect();
+    let insideCanvasMouseX = x - r.left;
+    let insideCanvasMouseY = y - r.top;
+    // do nothing if mouse outside of canvas ??? is it the behaviour i want ?
+    if (insideCanvasMouseX < 0 || insideCanvasMouseX > r.width) insideCanvasMouseX = undefined;
+    if (insideCanvasMouseY < 0 || insideCanvasMouseY > r.height) insideCanvasMouseY = undefined;
+
+    if (insideCanvasMouseX !== undefined && insideCanvasMouseY !== undefined) {
+        const cellIndexY = Math.floor(insideCanvasMouseY / cellSize);
+        const cellIndexX = Math.floor(insideCanvasMouseX / cellSize);
+        const currentPixel = pixelArray[cellIndexY][cellIndexX];
+        return currentPixel;
+    } else {
+        return undefined;
+    }
+}
+
+function handleMouse(e) {
+    if (e.type === "mousedown") {
+        isMouseDown = true;
+    } else if (e.type === "mouseup") {
+        isMouseDown = false;
+    }
+
+    if (!isMouseDown) return;
+
+    const currentPixel = getPixelFromWindowCoords(e.clientX, e.clientY);
+    if (!currentPixel) return; // mouse out of grid
+
+    if (isShiftDown) {
+        if (currentPixel.type === "start") {
+            isStartSet = false;
+            startRadio.disabled = false;
+        } else if (currentPixel.type === "end") {
+            isEndSet = false;
+            endRadio.disabled = false;
+        }
+        currentPixel.type = "free";
+        return;
+    }
+
+    if (currentPixel.type === "start" || currentPixel.type === "end") return; // do not override start and end
+
+    const radioType = getActiveRadio().value;
+
+    // paint cell type
+    if (radioType === "start") {
+        if (!isStartSet) {
+            currentPixel.type = radioType;
+            isStartSet = true;
+            startRadio.disabled = true;
+            // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
+            document.querySelectorAll('.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)')[0].checked = true;
+        }
+    } else if (radioType === "end") {
+        if (!isEndSet) {
+            currentPixel.type = radioType;
+            isEndSet = true;
+            endRadio.disabled = true;
+            // WATCH OUT, list could be empty (ok as long as i don't disable the obstacle radio)
+            document.querySelectorAll('.control-panel>div>input[name="cell_type"]:not(:checked):not(:disabled)')[0].checked = true;
+        }
+    } else {
+        currentPixel.type = radioType;
+        return;
+    }
 }
